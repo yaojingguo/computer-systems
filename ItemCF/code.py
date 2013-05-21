67d1c4b29f371b2546940b8593428f8df3d4afe7
Jingguo Yao <yaojingguo@gmail.com>
2013-05-21 10:04:40 +0800

    Update

--- ItemCF/code.py
+++ ItemCF/code.py
@@ -1,5 +1,7 @@
 import math
 
+import scipy.stats
+
 def pearsonr(x, y):
   n = len(x)
   sum_x = sum(x)
@@ -12,5 +14,15 @@ def pearsonr(x, y):
   return numerator / denominator
 
 if __name__ == "__main__":
-  print pearsonr([2.0, 7.0], [2.0, 1.0])
-  print pearsonr([10.0, 3.0, 7.0], [190.0, 50.0, 23.0])
+  x = [2.0, 7.0]
+  y = [2.0, 1.0]
+  print pearsonr(x, y)
+  print scipy.stats.pearsonr(x, y)
+  x = [1.0, 2.0, 3.0]
+  y = [1.0, 5.0, 7.0]
+  print pearsonr(x, y)
+  print scipy.stats.pearsonr(x, y)
+  x = [1.0, 20.0, 3.0, 100.9]
+  y = [1.0, 5.0, 70.0, 40.1]
+  print pearsonr(x, y)
+  print scipy.stats.pearsonr(x, y)
